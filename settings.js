bf49585268c00664d35897abd4358687c08f2fa7
Xmaduwa <xmaduwa82@gmail.com>
2025-05-04 06:33:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=Uoh0VQIC#xbSK-4xC-dxPEPighHA-j6ctAETuaocqFCBdgrUlAwo",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
